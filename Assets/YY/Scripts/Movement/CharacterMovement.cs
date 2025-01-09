d4e4a1c85a3561b049e1f08c899e82a2780773bf
Yusuf Yilmaz <yilmazyusuf2834@gmail.com>
2025-01-09 17:08:09 +0300

    .

---
+++ Assets/YY/Scripts/Movement/CharacterMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YY
+{
+    public class CharacterMovement : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+        void Update()
+        {
+
+        }
+    }
+}
